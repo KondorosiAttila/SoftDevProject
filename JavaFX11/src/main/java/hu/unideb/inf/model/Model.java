1d8ec438f8ac69d5b8a41b3048283f134f3033c6
KondorosiAttila <61154662+KondorosiAttila@users.noreply.github.com>
2020-04-09 12:37:33 +0200

    Add files via upload

---
+++ JavaFX11/src/main/java/hu/unideb/inf/model/Model.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.unideb.inf.model;
+
+import java.time.LocalDate;
+
+/**
+ * HERE
+ * testing the Git 
+ * here is my comment <--------------
+ * @author hallgato
+ */
+public class Model {
+
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Model() {
+        student = new Student("Robert Smith", 55, LocalDate.parse("1991-12-03"));
+    }
+}
