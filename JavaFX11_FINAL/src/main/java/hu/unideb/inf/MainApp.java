c815964cf72dae2aa7532f539df7dad3c9470a1d
Kondorosi Attila <tisaametalos@gmail.com>
2020-05-12 17:02:25 +0200

    CSS hozaadva

--- JavaFX11_FINAL/src/main/java/hu/unideb/inf/MainApp.java
+++ JavaFX11_FINAL/src/main/java/hu/unideb/inf/MainApp.java
@@ -6,6 +6,7 @@ import hu.unideb.inf.view.FXMLStudentsSceneController;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,6 +17,10 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/FXMLOrderScene.fxml"));
         Scene scene = new Scene(loader.load());
+        
+        //Scene style = new Scene(new Group(), 760, 414);
+        //style.getStylesheets().add("/css/app.css");
+        
         stage.setTitle("Pizza Kingz");
         stage.setScene(scene);
         stage.show();
